b19d74ec183c9e5e0a4ee6524e519fb1e552548d
Tejas <56431505+tejasvtanna@users.noreply.github.com>
2020-04-30 15:14:46 +0530

    Updates

---
+++ precache-manifest.42cb1b1ce0d958ffe4ad503df499ccaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9604664f4e14af16bfca82e88d4ca2c",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "e7a0e121e10c7b688251",
+    "url": "/covid-19-tracker/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "b03244060e49c6eb6952",
+    "url": "/covid-19-tracker/static/css/main.2f8911a2.chunk.css"
+  },
+  {
+    "revision": "e7a0e121e10c7b688251",
+    "url": "/covid-19-tracker/static/js/2.f0b82260.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/covid-19-tracker/static/js/2.f0b82260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03244060e49c6eb6952",
+    "url": "/covid-19-tracker/static/js/main.33582ba0.chunk.js"
+  },
+  {
+    "revision": "7119b1d8aff41a893625",
+    "url": "/covid-19-tracker/static/js/runtime-main.c56aa68b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-live-stats/precache-manifest.d299b930c9dab142aa902ddf1c2adbd8.js"
+  "/covid-19-tracker/precache-manifest.42cb1b1ce0d958ffe4ad503df499ccaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19-live-stats/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
